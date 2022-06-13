11983e9cbb45d286b8d7d941b24b0c191c953b4c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-06-12 22:51:07 -0700

    remove button is removing the movie from the watchlist but i need to figure out how to re render the page once the remove button is clicked with the updated list

--- watchlist.js
+++ watchlist.js
@@ -1,158 +1,85 @@
 import { addList, apiKey } from './index.js'
 
+let removeBtn;
 
-console.log(addList)
 
+function render2(){
 
-let html = ""
-Promise.all(
-                addList.map(movie => 
-                    fetch(`https://www.omdbapi.com/?i=${movie}&apikey=${apiKey}`)
-                    .then(res => res.json())
-                    .catch(err => console.log(err)))   
-                    ).then(movies => {
-                        // console.log(movies)
-                        for(let i = 0; i < movies.length; i++){
-                            console.log(movies[i].Title) 
-                            if(movies[i].Ratings.length > 0){
-                                
-                                html += `
-                                <div class="movie">
-                                    <img src=${movies[i].Poster} class="movie-poster" alt="poster missing"/>
-                                    <div class="movie-info">
-                                        <div class="movie-header">
-                                            <h2 class="movie-title"> ${movies[i].Title} </h2>
-                                            <p class="movie-rating"> ⭐ ${movies[i].Ratings[0].Value} </p>
+    let html = ""
+    Promise.all(
+                    addList.map(movie => 
+                        fetch(`https://www.omdbapi.com/?i=${movie}&apikey=${apiKey}`)
+                        .then(res => res.json())
+                        .catch(err => console.log(err)))   
+                        ).then(movies => {
+                            // console.log(movies)
+                            for(let i = 0; i < movies.length; i++){
+                                console.log(movies[i].Title) 
+                                if(movies[i].Ratings.length > 0){
+                                    
+                                    html += `
+                                    <div class="movie">
+                                        <img src=${movies[i].Poster} class="movie-poster" alt="poster missing"/>
+                                        <div class="movie-info">
+                                            <div class="movie-header">
+                                                <h2 class="movie-title"> ${movies[i].Title} </h2>
+                                                <p class="movie-rating"> ⭐ ${movies[i].Ratings[0].Value} </p>
+                                            </div>
+                                            <div class="details-add">
+                                                <p class="movie-details"> ${movies[i].Runtime} </p>
+                                                <p class="movie-details"> ${movies[i].Genre} </p>
+                                                <p class="movie-details"> <a href="#" class="remove-movie" id=${movies[i].imdbID}> Remove from list </a>
+                                            </div>
+                                            <p class="movie-plot"> ${movies[i].Plot} </p>
                                         </div>
-                                        <div class="details-add">
-                                            <p class="movie-details"> ${movies[i].Runtime} </p>
-                                            <p class="movie-details"> ${movies[i].Genre} </p>
-                                            <p class="movie-details"> <a href="#" class="add-movie" id=${movies[i].imdbID}> Add to list </a>
-                                        </div>
-                                        <p class="movie-plot"> ${movies[i].Plot} </p>
                                     </div>
-                                </div>
-                                <div class="separator"></div>
-                                `
-                            } else {
-                                html += `
-                                <div class="movie">
-                                    <img src=${movies[i].Poster} class="movie-poster" alt="poster missing" />
-                                    <div class="movie-info">
-                                        <div class="movie-header">
-                                            <h2 class="movie-title"> ${movies[i].Title} </h2>
-                                            <p class="movie-rating"> no rating yet </p>
+                                    <div class="separator"></div>
+                                    `
+                                } else {
+                                    html += `
+                                    <div class="movie">
+                                        <img src=${movies[i].Poster} class="movie-poster" alt="poster missing" />
+                                        <div class="movie-info">
+                                            <div class="movie-header">
+                                                <h2 class="movie-title"> ${movies[i].Title} </h2>
+                                                <p class="movie-rating"> no rating yet </p>
+                                            </div>
+                                            <div class="details-add">
+                                                <p class="movie-details"> ${movies[i].Runtime} </p>
+                                                <p class="movie-details"> ${movies[i].Genre} </p>
+                                                <p class="movie-details"> <a href="#" class="remove-movie" id=${movies[i].imdbID}> Remove from list </a>
+                                            </div>
+                                            <p class="movie-plot"> ${movies[i].Plot} </p>
                                         </div>
-                                        <div class="details-add">
-                                            <p class="movie-details"> ${movies[i].Runtime} </p>
-                                            <p class="movie-details"> ${movies[i].Genre} </p>
-                                            <p class="movie-details"> <a href="#" class="add-movie" id=${movies[i].imdbID}> Add to list </a>
-                                        </div>
-                                        <p class="movie-plot"> ${movies[i].Plot} </p>
                                     </div>
-                                </div>
-                                <div class="separator"></div>
-                                `
+                                    <div class="separator"></div>
+                                    `
+                                }
                             }
                             document.getElementById("movie-watchlist").innerHTML = html
-                        }
-                    })
-                    
+                            removeBtn = document.querySelectorAll(".remove-movie")
+                            removeMovie()
+                        })
+}
+
             
-// const apiKey = "c14ca80c"
-// let movie = document.getElementById("movie-search")
-// let addList = []
-// let movieArray = []
+
+function removeMovie(){
+    for(let button of removeBtn){
+        button.addEventListener("click", function(e){
+            e.preventDefault()
+            let index = addList.indexOf(button.id)
+            console.log(index)
+            if(index > -1){
+                addList.splice(index, 1)
+                localStorage.setItem("movieIds", JSON.stringify(addList))
+                console.log(addList)
+            }
+        })
+    }
+}
+
+// addList = JSON.parse(localStorage.getItem("movieIds"))
 
 
-
-// document.getElementById("search").addEventListener("submit", async function(e){
-//     e.preventDefault()
-//     let html = ""
-//     const res = await fetch(`http://www.omdbapi.com/?s=${movie.value}&apikey=${apiKey}`)
-//     const data = await res.json()    
-
-//             for(let i = 0; i < data.Search.length; i++){
-//                 movieArray.push(data.Search[i].imdbID)
-
-//             }
-//             console.log(movieArray)
-
-         
-//         let movieId;
-            
-//             Promise.all(
-//                 movieArray.map(movie => 
-//                     fetch(`http://www.omdbapi.com/?i=${movie}&apikey=${apiKey}`)
-//                     .then(res => res.json())
-//                     .catch(err => console.log(err)))   
-//                     ).then(movies => {
-//                         // console.log(movies)
-//                         for(let i = 0; i < movies.length; i++){
-//                             // console.log(movies[i]) 
-//                             if(movies[i].Ratings.length > 0){
-                                
-//                                 html += `
-//                                 <div class="movie">
-//                                     <img src=${movies[i].Poster} class="movie-poster" alt="poster missing"/>
-//                                     <div class="movie-info">
-//                                         <div class="movie-header">
-//                                             <h2 class="movie-title"> ${movies[i].Title} </h2>
-//                                             <p class="movie-rating"> ⭐ ${movies[i].Ratings[0].Value} </p>
-//                                         </div>
-//                                         <div class="details-add">
-//                                             <p class="movie-details"> ${movies[i].Runtime} </p>
-//                                             <p class="movie-details"> ${movies[i].Genre} </p>
-//                                             <p class="movie-details"> <a href="#" class="add-movie" id=${movies[i].imdbID}> Add to list </a>
-//                                         </div>
-//                                         <p class="movie-plot"> ${movies[i].Plot} </p>
-//                                     </div>
-//                                 </div>
-//                                 <div class="separator"></div>
-//                                 `
-//                             } else {
-//                                 html += `
-//                                 <div class="movie">
-//                                     <img src=${movies[i].Poster} class="movie-poster" alt="poster missing" />
-//                                     <div class="movie-info">
-//                                         <div class="movie-header">
-//                                             <h2 class="movie-title"> ${movies[i].Title} </h2>
-//                                             <p class="movie-rating"> no rating yet </p>
-//                                         </div>
-//                                         <div class="details-add">
-//                                             <p class="movie-details"> ${movies[i].Runtime} </p>
-//                                             <p class="movie-details"> ${movies[i].Genre} </p>
-//                                             <p class="movie-details"> <a href="#" class="add-movie" id=${movies[i].imdbID}> Add to list </a>
-//                                         </div>
-//                                         <p class="movie-plot"> ${movies[i].Plot} </p>
-//                                     </div>
-//                                 </div>
-//                                 <div class="separator"></div>
-//                                 `
-//                             }
-//                         }
-                        
-//                 document.getElementById("movie-list").innerHTML = html
-//                 let addMovieBtn = document.querySelectorAll(".add-movie")
-            
-//                 for(let i = 0; i < addMovieBtn.length; i++){
-//                     addMovieBtn[i].addEventListener("click", function(e){
-//                         e.preventDefault()
-//                         if(!addList.includes(addMovieBtn[i].id)){
-//                             addList.push(addMovieBtn[i].id)
-//                             localStorage.setItem("movieIds", JSON.stringify(addList))
-//                         }
-//                         console.log(addList)
-//                     })
-
-//                 }
-//             })
-//                 movieArray = []
-//     })
-
-
-    
-//     addList = JSON.parse(localStorage.getItem("movieIds"))
-//     console.log(addList)
-
-// export {addList}
+render2()
